3f4924f40506785788ae8c9fd763b4f9c46e8159
Costa Saridis <kostassaridis@hotmail.com>
2021-11-25 14:24:26 +0100

    sad

--- create_DB.py
+++ create_DB.py
@@ -18,7 +18,6 @@ def create_db(engine):
     # Create a primary key on the table
     query = """ALTER TABLE daily_prices 
                 ADD PRIMARY KEY (symbol, date);"""
-
     engine.execute(query)
- 
+   
 
